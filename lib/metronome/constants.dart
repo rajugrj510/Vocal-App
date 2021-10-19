b34efe72f1a72d16f70c5f34e01633bbce6a7c50
rajugrj510 <rajugrj510@gmail.com>
2021-10-18 22:36:59 -0500

    Slider and Tap button added

--- lib/metronome/constants.dart
+++ lib/metronome/constants.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-const ktext_style =  TextStyle(
-      color: Colors.white,
-      fontSize: 40.0,
-      fontWeight: FontWeight.bold,
-    );
+const kactiveSliderTrackColor = Color(0xFF1565C0);
+const kthumbColor = Color(0xFF42A5F5);
+
+const kactiveSliderHeight = 8.0;
+const kinactiveSliderHeight = 8.0;
+
+const ktextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+);
+const kdivisionTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 15.0,
+  fontWeight: FontWeight.bold,
+);
--- lib/metronome/metronomes.dart
+++ lib/metronome/metronomes.dart
@@ -1,17 +1,23 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
-import 'package:vocal_app/metronome/constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:vocal_app/metronome/constants.dart';
+import 'Round_Icon_Button.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
 import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class Metronome extends StatefulWidget {
-
   @override
   _MetronomeState createState() => _MetronomeState();
 }
 
 class _MetronomeState extends State<Metronome> {
-  int tempo = 90;
-  int _value = 0 ;
+  int tempo = 70;
+  double value = 99;
+  double temp = 1.0;
+  String meter = '4/4';
+  String subdiv = 'SUB/DIV';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,44 +45,190 @@ class _MetronomeState extends State<Metronome> {
       ),
       body: Column(
         children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              margin: EdgeInsets.all(35.0),
-              decoration: BoxDecoration(
+          //This is the top container for Metronome number
+          Container(
+            height: 200.0,
+            width: 600.0,
+            margin: EdgeInsets.fromLTRB(17.0, 40.0, 30.0, 30.0),
+            decoration: BoxDecoration(
                 color: Color(0xFFEE0708),
                 borderRadius: BorderRadius.circular(15.0),
                 boxShadow: [
                   BoxShadow(
-                  color: Colors.black,
-                  blurRadius: 5,
-                  offset: Offset(3, 8),
-                )]
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(30.0),
+                    color: Colors.black,
+                    blurRadius: 5,
+                    offset: Offset(3, 8),
+                  )
+                ]),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 120.0,
+                    width: 200.0,
+                    margin:
+                        EdgeInsets.only(left: 15.0, top: 45.0, bottom: 15.0),
                     decoration: BoxDecoration(
                       color: Colors.blueAccent,
                       borderRadius: BorderRadius.circular(15.0),
                     ),
-                    child: Padding(
-                      padding:  EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
                       child: Text(
-                        tempo.toString(),
+                        tempo.toString(), //tempo number in the widget
                         style: TextStyle(
-                          fontSize: 70,
+                          fontSize: 80,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-
-
-                ],
-              ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                // Row of vertical slider.
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                            width: 45.0,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(7.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                meter, //The meter data show
+                                style: kdivisionTextStyle,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(7.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: Text(
+                                subdiv,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 145.0,
+                      //This is the row of slider in vertical direction.
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SfSliderTheme(
+                            data: SfSliderThemeData(
+                              thumbColor: Colors.blue[200],
+                              activeTrackHeight: kactiveSliderHeight,
+                              inactiveTrackHeight: kinactiveSliderHeight,
+                              activeTrackColor: kactiveSliderTrackColor,
+                              inactiveTrackColor: Colors.black,
+                              trackCornerRadius: -1.0,
+                            ),
+                            child: SfSlider.vertical(
+                              min: 0.0,
+                              max: 100.0,
+                              value: tempo,
+                              onChanged: (newVal) {
+                                setState(() {
+                                  tempo = newVal.toInt();
+                                });
+                              },
+                            ),
+                          ),
+                          SfSliderTheme(
+                            data: SfSliderThemeData(
+                              thumbColor: Colors.blue[200],
+                              activeTrackHeight: kactiveSliderHeight,
+                              inactiveTrackHeight: kinactiveSliderHeight,
+                              activeTrackColor: kactiveSliderTrackColor,
+                              inactiveTrackColor: Colors.black,
+                              trackCornerRadius: -1.0,
+                            ),
+                            child: SfSlider.vertical(
+                              min: 0.0,
+                              max: 100.0,
+                              value: tempo,
+                              onChanged: (newVal) {
+                                setState(() {
+                                  tempo = newVal.toInt();
+                                });
+                              },
+                            ),
+                          ),
+                          SfSliderTheme(
+                            data: SfSliderThemeData(
+                              thumbColor: Colors.blue[200],
+                              activeTrackHeight: kactiveSliderHeight,
+                              inactiveTrackHeight: kinactiveSliderHeight,
+                              activeTrackColor: kactiveSliderTrackColor,
+                              inactiveTrackColor: Colors.black,
+                              trackCornerRadius: -1.0,
+                            ),
+                            child: SfSlider.vertical(
+                              min: 0.0,
+                              max: 100.0,
+                              value: tempo,
+                              onChanged: (newVal) {
+                                setState(() {
+                                  tempo = newVal.toInt();
+                                });
+                              },
+                            ),
+                          ),
+                          SfSliderTheme(
+                            data: SfSliderThemeData(
+                              thumbColor: Colors.blue[200],
+                              activeTrackHeight: kactiveSliderHeight,
+                              inactiveTrackHeight: kinactiveSliderHeight,
+                              activeTrackColor: kactiveSliderTrackColor,
+                              inactiveTrackColor: Colors.black,
+                              trackCornerRadius: -1.0,
+                            ),
+                            child: SfSlider.vertical(
+                              min: 0.0,
+                              max: 100.0,
+                              value: tempo,
+                              onChanged: (newVal) {
+                                setState(() {
+                                  tempo = newVal.toInt();
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
           //This is the play button
@@ -94,20 +246,21 @@ class _MetronomeState extends State<Metronome> {
                       IconButton(
                         onPressed: () {},
                         icon: Icon(
-                            Icons.arrow_back_ios,
-                          size: 30.0,),
+                          Icons.arrow_back_ios,
+                          size: 30.0,
+                        ),
                       ),
                       IconButton(
                         onPressed: () {},
                         icon: Icon(Icons.play_arrow),
                         iconSize: 100.0,
-
                       ),
                       IconButton(
                         onPressed: () {},
                         icon: Icon(
-                            Icons.arrow_forward_ios,
-                        size: 30.0,),
+                          Icons.arrow_forward_ios,
+                          size: 30.0,
+                        ),
                       ),
                     ],
                   ),
@@ -122,10 +275,11 @@ class _MetronomeState extends State<Metronome> {
           //This is where the last set of buttons will be for the metronome
           Expanded(
             child: Row(
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                //This container is for the tap button
                 Container(
-                  decoration:BoxDecoration(
+                  decoration: BoxDecoration(
                     color: Colors.blueAccent,
                     borderRadius: BorderRadius.circular(15.0),
                   ),
@@ -141,39 +295,48 @@ class _MetronomeState extends State<Metronome> {
                     ),
                   ),
                 ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                //This container is for the increase and decrease icon.
                 Container(
-                  //padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
                   decoration: BoxDecoration(
                     color: Colors.grey.shade800,
                     borderRadius: BorderRadius.circular(15.0),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      RoundIconButton(
-                          icon: FontAwesomeIcons.minus,
-                          onPressed: (){
-                            //perform action for metronome
-                            setState(() {
-                              tempo--;
-                            });
-                          }),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                        tempo.toString(),
-                        style: ktext_style,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RoundIconButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: () {
+                              //perform action for metronome
+                              setState(() {
+                                tempo--;
+                              });
+                            }),
+                        Container(
+                          width: 40.0,
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(
+                              tempo.toString(),
+                              style: ktextStyle,
+                            ),
+                          ),
                         ),
-                      ),
-                      RoundIconButton(
-                          icon: FontAwesomeIcons.plus,
-                          onPressed: (){
-                            //perform action for metronome
-                            setState(() {
-                              tempo++;
-                            });
-                          }),
-                    ],
+                        RoundIconButton(
+                            icon: FontAwesomeIcons.plus,
+                            onPressed: () {
+                              //perform action for metronome
+                              setState(() {
+                                tempo++;
+                              });
+                            }),
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -182,25 +345,5 @@ class _MetronomeState extends State<Metronome> {
         ],
       ),
     );
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({required this.icon, required this.onPressed});
-  final IconData icon;
-  final VoidCallback onPressed;
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-        child: Icon(icon,
-        color: Colors.white,),
-        elevation: 3.0,
-        shape: CircleBorder(),
-        fillColor: Colors.black,
-        constraints: BoxConstraints.tightFor(
-          width: 40.0,
-          height: 40.0,
-        ),
-        onPressed: onPressed);
   }
 }
