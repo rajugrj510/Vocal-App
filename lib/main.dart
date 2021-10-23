7fdcc49daa73393a1f95689046d27709201a531f
Steven Sterlacci <93019998+LegendofSteven@users.noreply.github.com>
2021-10-23 17:12:38 -0500

    Updated to Add Piano functionality
    Known Issues: Placed in vertical position rather than horizontal position
    
    NOTE: Because Piano.SF2 is 112 MB I can't commit it so I will upload the file onto Discord with instructions on how to implement it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'metronome/metronomes.dart';
+import 'piano/piano.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Metronome(),
+    home: Piano(),
   ));
 }
 
