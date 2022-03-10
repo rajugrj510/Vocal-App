4ebbe61c35b949e36ed6b4bf88b0f1789c1dee38
tnd002 <86018193+tnd002@users.noreply.github.com>
2022-03-10 05:23:43 -0600

    Add files via upload

--- lib/piano.dart
+++ lib/piano.dart
@@ -1,54 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_midi/flutter_midi.dart';
-import 'dart:typed_data';
 import 'package:flutter/services.dart';
-
-enum KeyColor { WHITE, BLACK }
-
-class PianoKey extends StatelessWidget {
-  final KeyColor color;
-  final double width;
-  final int midiNote;
-  final FlutterMidi flutterMidi;
-
-  const PianoKey.white({
-    required this.width,
-    required this.midiNote,
-    required this.flutterMidi,
-  }) : this.color = KeyColor.WHITE;
-
-  const PianoKey.black({
-    required this.width,
-    required this.midiNote,
-    required this.flutterMidi,
-  }) : this.color = KeyColor.BLACK;
-
-  playNote() {
-    flutterMidi.playMidiNote(midi: midiNote);
-  }
-
-  stopNote() {
-    flutterMidi.stopMidiNote(midi: midiNote);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (_) => playNote(),
-      onTapUp: (_) => stopNote(),
-      child: Container(
-        height: width,
-        decoration: BoxDecoration(
-          color: color == KeyColor.WHITE ? Colors.white : Colors.black,
-          border: Border.all(color: Colors.black, width: 2),
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
-        ),
-      ),
-    );
-  }
-}
+import 'package:flutter_midi/flutter_midi.dart';
+import 'package:tonic/tonic.dart';
 
 class Piano extends StatefulWidget {
   const Piano({Key? key}) : super(key: key);
@@ -58,7 +11,6 @@ class Piano extends StatefulWidget {
 }
 
 class _PianoState extends State<Piano> {
-  final octaveNumber = 3;
   final FlutterMidi flutterMidi = FlutterMidi();
 
   @override
@@ -69,122 +21,139 @@ class _PianoState extends State<Piano> {
 
   Future<void> setupMIDIPlugin() async {
     flutterMidi.unmute();
-    ByteData _byte =
-        await rootBundle.load('assets/Yamaha-Grand-Lite-SF-v1.1.sf2');
+    ByteData _byte = await rootBundle.load('assets/piano.sf2');
     flutterMidi.prepare(sf2: _byte);
   }
 
-  get octaveStartingNote => (octaveNumber * 12) % 128;
+  double get keyWidth => 80 + (80 * _widthRatio);
+  double _widthRatio = 0.0;
+  bool _showLabels = true;
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final whiteKeySize = constraints.maxHeight / 7;
-        final blackKeySize = whiteKeySize / 2;
-        return Stack(
-          children: [
-            _buildWhiteKeys(whiteKeySize),
-            _buildBlackKeys(constraints.maxWidth, blackKeySize, whiteKeySize),
-          ],
-        );
-      },
+    return MaterialApp(
+      title: 'Vocal App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+          appBar: AppBar(title: Text("Piano")),
+          drawer: Drawer(
+              child: SafeArea(
+                  child: ListView(children: <Widget>[
+            Container(height: 20.0),
+            ListTile(title: Text("Change Width")),
+            Slider(
+                activeColor: Colors.redAccent,
+                inactiveColor: Colors.white,
+                min: 0.0,
+                max: 1.0,
+                value: _widthRatio,
+                onChanged: (double value) =>
+                    setState(() => _widthRatio = value)),
+            Divider(),
+            ListTile(
+                title: Text("Show Labels"),
+                trailing: Switch(
+                    value: _showLabels,
+                    onChanged: (bool value) =>
+                        setState(() => _showLabels = value))),
+            Divider(),
+          ]))),
+          body: ListView.builder(
+            itemCount: 7,
+            scrollDirection: Axis.horizontal,
+            controller: ScrollController(initialScrollOffset: 1500.0),
+            itemBuilder: (BuildContext context, int index) {
+              final int i = index * 12;
+              return SafeArea(
+                child: Stack(children: <Widget>[
+                  Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    _buildKey(24 + i, false),
+                    _buildKey(26 + i, false),
+                    _buildKey(28 + i, false),
+                    _buildKey(29 + i, false),
+                    _buildKey(31 + i, false),
+                    _buildKey(33 + i, false),
+                    _buildKey(35 + i, false),
+                  ]),
+                  Positioned(
+                      left: 0.0,
+                      right: 0.0,
+                      bottom: 100,
+                      top: 0.0,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(width: keyWidth * .5),
+                            _buildKey(25 + i, true),
+                            _buildKey(27 + i, true),
+                            Container(width: keyWidth),
+                            _buildKey(30 + i, true),
+                            _buildKey(32 + i, true),
+                            _buildKey(34 + i, true),
+                            Container(width: keyWidth * .5),
+                          ])),
+                ]),
+              );
+            },
+          )),
     );
   }
 
-  _buildWhiteKeys(double whiteKeySize) {
-    return Column(
-      children: [
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote + 11,
-          flutterMidi: flutterMidi,
-        ),
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote + 9,
-          flutterMidi: flutterMidi,
-        ),
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote + 7,
-          flutterMidi: flutterMidi,
-        ),
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote + 5,
-          flutterMidi: flutterMidi,
-        ),
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote + 4,
-          flutterMidi: flutterMidi,
-        ),
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote + 2,
-          flutterMidi: flutterMidi,
-        ),
-        PianoKey.white(
-          width: whiteKeySize,
-          midiNote: octaveStartingNote,
-          flutterMidi: flutterMidi,
-        ),
+  Widget _buildKey(int midi, bool accidental) {
+    playNote() {
+      flutterMidi.playMidiNote(midi: midi);
+    }
+
+    stopNote() {
+      flutterMidi.stopMidiNote(midi: midi);
+    }
+
+    final pitchName = Pitch.fromMidiNumber(midi).toString();
+    final pianoKey = Stack(
+      children: <Widget>[
+        Semantics(
+            button: true,
+            hint: pitchName,
+            child: Material(
+                borderRadius: borderRadius,
+                color: accidental ? Colors.black : Colors.white,
+                child: InkWell(
+                  borderRadius: borderRadius,
+                  highlightColor: Colors.grey,
+                  onTap: () {},
+                  onTapDown: (_) => playNote(),
+                ))),
+        Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 20.0,
+            child: _showLabels
+                ? Text(pitchName,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: !accidental ? Colors.black : Colors.white))
+                : Container()),
       ],
     );
-  }
-
-  _buildBlackKeys(
-      double pianoHeight, double blackKeySize, double whiteKeySize) {
+    if (accidental) {
+      return Container(
+          width: keyWidth,
+          margin: EdgeInsets.symmetric(horizontal: 2.0),
+          padding: EdgeInsets.symmetric(horizontal: keyWidth * .1),
+          child: Material(
+              elevation: 6.0,
+              borderRadius: borderRadius,
+              shadowColor: Color(0x802196F3),
+              child: pianoKey));
+    }
     return Container(
-      width: pianoHeight * 0.55,
-      child: Column(
-        children: [
-          SizedBox(
-            height: whiteKeySize - blackKeySize / 2,
-          ),
-          PianoKey.black(
-            width: blackKeySize,
-            midiNote: octaveStartingNote + 10,
-            flutterMidi: flutterMidi,
-          ),
-          SizedBox(
-            height: whiteKeySize - blackKeySize,
-          ),
-          PianoKey.black(
-            width: blackKeySize,
-            midiNote: octaveStartingNote + 8,
-            flutterMidi: flutterMidi,
-          ),
-          SizedBox(
-            height: whiteKeySize - blackKeySize,
-          ),
-          PianoKey.black(
-            width: blackKeySize,
-            midiNote: octaveStartingNote + 6,
-            flutterMidi: flutterMidi,
-          ),
-          SizedBox(
-            height: whiteKeySize,
-          ),
-          SizedBox(
-            height: whiteKeySize - blackKeySize,
-          ),
-          PianoKey.black(
-            width: blackKeySize,
-            midiNote: octaveStartingNote + 3,
-            flutterMidi: flutterMidi,
-          ),
-          SizedBox(
-            height: whiteKeySize - blackKeySize,
-          ),
-          PianoKey.black(
-            width: blackKeySize,
-            midiNote: octaveStartingNote + 1,
-            flutterMidi: flutterMidi,
-          ),
-        ],
-      ),
-    );
+        width: keyWidth,
+        child: pianoKey,
+        margin: EdgeInsets.symmetric(horizontal: 2.0));
   }
 }
+
+const BorderRadius borderRadius = BorderRadius.only(
+    bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0));
