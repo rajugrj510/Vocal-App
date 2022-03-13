29ce16f9485b78c5e8e45dee0d93c3c464243f15
mrn0089 <81244517+mrn0089@users.noreply.github.com>
2022-03-13 11:36:18 -0500

    adding audio recorder lib
    will have to change name of main.dart

---
+++ api/sound_player.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:flutter_sound_lite/public/flutter_sound_player.dart';
+
+final pathToReadAudio = 'audio_example.aac';
+
+class SoundPlayer {
+  FlutterSoundPlayer? _audioPlayer;
+
+  bool get isPlaying => _audioPlayer!.isPlaying;
+
+  Future init() async {
+    _audioPlayer = FlutterSoundPlayer();
+
+    await _audioPlayer!.openAudioSession();
+  }
+
+  void dispose() {
+    _audioPlayer!.closeAudioSession();
+    _audioPlayer = null;
+  }
+
+  Future _play(VoidCallback whenFinished) async {
+    await _audioPlayer!.startPlayer(
+      fromURI: pathToReadAudio,
+      whenFinished: whenFinished,
+    );
+  }
+
+  Future _stop() async {
+    await _audioPlayer!.stopPlayer();
+  }
+
+  Future togglePlaying({required VoidCallback whenFinished}) async {
+    if (_audioPlayer!.isStopped) {
+      await _play(whenFinished);
+    } else {
+      await _stop();
+    }
+  }
+}
